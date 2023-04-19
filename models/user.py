ed12f23bbfb3fb39ce78cb3b2de675d4240381d4
JoshAmpofo <joshampofo92@gmail.com>
2023-04-19 19:29:03 +0000

    Modify user props for db_storage

--- models/user.py
+++ models/user.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This module defines a class User using db_storage as engine"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """Defines User class with the following attributes
+
+    email(str): user login email
+    password(str): user login password
+    first_name(str): first name of user
+    last_name(str): last name of user
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
