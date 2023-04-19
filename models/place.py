d66b9ba43fa1750a887f5a0a1af8407f41cdfb93
JoshAmpofo <joshampofo92@gmail.com>
2023-04-19 19:47:48 +0000

    Modify place class for DBStorage engine

--- models/place.py
+++ models/place.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+"""Define Place class for Airbnb console"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+class Place(BaseModel, Base):
+    """defines place attributes for user
+
+    city_id(str): id of city where place is located
+    user_id(str): id of user 
+    name (str): name of place
+    description(str): brief description of place
+    number_rooms(int): number of rooms in place
+    number_bathrooms(int): number of bathrooms in place
+    max_guest(int): maximum number of guests place can accommodate
+    price_by_night(int): unit price of place per night
+    latitude(float): GPS coordinates of place
+    longitude(float): GPS coordinates of place
+    """
+    
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
