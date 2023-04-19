519185f3d4c6a8e4a7636d6ba90b6c3018500b37
JoshAmpofo <joshampofo92@gmail.com>
2023-04-19 09:10:30 +0000

    Modify city class for DBstorage

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """Define City class
+
+    Args:
+        __tablename__: db object class is linked to
+        state_id(str): id of state, city belongs to
+        name(str): name of city
+    """
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
