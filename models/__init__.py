ef324dba91b7c5a2fc2dcb11f4ea0a92d36b2fe3
JoshAmpofo <joshampofo92@gmail.com>
2023-04-19 13:09:59 +0000

    Modify storage parameters

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""Initialize both file storage systems"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,6 +9,13 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+import os
 
-storage = FileStorage()
+
+env = getenv('HBNB_TYPE_STORAGE')
+
+if env == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
