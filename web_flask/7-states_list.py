5f0085d6f580dd74cbb9a7c81e186cb6a205ce26
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2023-05-24 09:26:12 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""Flask app to generate html list of all states from storage"""
-from flask import Flask, render_template
+"""Start web application with two routings
+"""
+
 from models import storage
-app = Flask('web_flask')
-app.url_map.strict_slashes = False
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
 @app.route('/states_list')
-def list_of_states():
-    """Render html with unordered list of states from `storage`"""
-    states = sorted(storage.all('State').values(), key=lambda s: s.name)
-    return render_template('7-states_list.html', states=states)
+def states_list():
+    """Render template with states
+    """
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown_db(*args, **kwargs):
-    """Close database or file storage"""
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
